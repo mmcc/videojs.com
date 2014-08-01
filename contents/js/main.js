33414961ae58b48949a31ba8ffbdd06f7f0e83fe
Matt McClure <matt@htbx.net>
2014-08-01 00:34:34 -0700

    added browserify

---
+++ contents/js/main.js
@@ -0,0 +1,8 @@
+var $ = require('jquery');
+var ScrollMagic = require('scrollmagic')($);
+
+var controller = new ScrollMagic();
+
+// var scene = new ScrollScene({triggerElement: ".video-preview", duration: 300})
+// 						      .setPin("#example")
+// 						      .addTo(controller);
